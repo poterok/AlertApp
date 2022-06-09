aca440404c052ab1c66f7da032dcaf8df808713c
poreok <poterok@gmail.com>
2022-06-10 00:54:50 +0300

    Добавил обработку ошибки
    Добавил обработку ошибки

--- AlertApp/Model/MainAlert.swift
+++ AlertApp/Model/MainAlert.swift
@@ -10,37 +10,56 @@ import UIKit
 
 
 class MainAlert {
-    
-    
-    
-    func mainAlert (title:String, description:String, style: UIAlertController.Style, viewController:ViewController) {
+///MARK: Указываем имя
+    func mainAlert (title:String, description:String, style: UIAlertController.Style, viewController: UIViewController, completion: @escaping (String) -> Void ) {
+        
         let alert = UIAlertController(title: title, message: description, preferredStyle: style)
         
         let action = UIAlertAction(title: "Set your name", style: .default) {(action) in
             let text = alert.textFields?.first?.text ?? ""
-                viewController.nameLabel.text! += text
+            completion(text)
         }
         alert.addTextField()
         alert.addAction(action)
         viewController.present(alert, animated: true)
     }
     
+}
+///MARK: Ищем сумму
+class FindeSumAlert:MainAlert {
     
-    func resultAlert (title:String, description:String, style: UIAlertController.Style, viewController:ViewController){
+    func resultAlert (title:String, description:String, style: UIAlertController.Style, viewController:UIViewController, completion: @escaping (String) -> Void ){
         let alert = UIAlertController(title: title, message: description, preferredStyle: style)
         let action2 = UIAlertAction(title: "Укажи 2 числа", style: .default) {(action2) in
             let text = alert.textFields?.first?.text ?? ""
             let text2 = alert.textFields?.last?.text ?? ""
             
-            let sum  = Int(text)! + Int(text2)!
-            viewController.sumLabel.text! += String(sum)
+            if (Int(text) != nil) && (Int(text2) != nil) {
+                print("Прверили")
+            } else {
+                let error = ErrorAlert()
+                error.resultAlert(title: "Ошибка", description: "Введите числа", style: .alert, viewController: viewController)
+                return
+            }
+            var sum  = Int(text)! + Int(text2)!
+            completion(String(sum))
         }
-            alert.addAction(action2)
-            alert.addTextField()
-            alert.addTextField()
-            
-            viewController.present(alert, animated: true)
-            
+        alert.addAction(action2)
+        alert.addTextField()
+        alert.addTextField()
+        viewController.present(alert, animated: true)
+           
+    }
+}
+
+
+class ErrorAlert {
+    func resultAlert (title:String, description:String, style: UIAlertController.Style,viewController:UIViewController) {
+        let alert = UIAlertController(title: title, message: description, preferredStyle: style)
+        let action = UIAlertAction(title: "ОК", style: .cancel)
+        alert.addAction(action)
+        viewController.present(alert, animated: true)
         
     }
 }
+
--- AlertApp/ViewController.swift
+++ AlertApp/ViewController.swift
@@ -11,19 +11,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var resultLabel: UIStackView!
+    @IBOutlet weak var sumLabel: UILabel!
+    @IBOutlet weak var guessLabel: UILabel!
     @IBOutlet weak var buttonStackLabel: UIStackView!
-    
-    
     @IBOutlet weak var guessButton: UIButton!
     @IBOutlet weak var findeSumButton: UIButton!
+    let alert = MainAlert()
+    let findeSumAlert = FindeSumAlert()
     
+///MARK:Алерт после загрузки
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        alert.mainAlert(title: "Привет", description: "Укажи свое имя", style: .alert, viewController: self) {
+            text1 in self.nameLabel.text! += text1
+        }
     }
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         guessButton.backgroundColor = .yellow
@@ -34,10 +37,10 @@ class ViewController: UIViewController {
         resultLabel.layer.cornerRadius = 10
         buttonStackLabel.layer.masksToBounds = true
         buttonStackLabel.layer.cornerRadius = 10
-        
-        
     }
-
-
+    @IBAction func findeSum(_ sender: Any) {
+        
+        findeSumAlert.resultAlert(title: "Cложи 2 числа", description: "Сожи", style: .alert, viewController: self) {  sum in self.sumLabel.text! = "Sum = \(sum)" }
+    }
 }
 
