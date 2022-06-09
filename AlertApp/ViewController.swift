cc789aedbf23d569274a64c750ef3850b4519913
poreok <poterok@gmail.com>
2022-06-09 22:10:38 +0300

    Добавил экран

--- AlertApp/ViewController.swift
+++ AlertApp/ViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var resultLabel: UIStackView!
+    @IBOutlet weak var buttonStackLabel: UIStackView!
+    
+    
+    @IBOutlet weak var guessButton: UIButton!
+    @IBOutlet weak var findeSumButton: UIButton!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        guessButton.backgroundColor = .yellow
+        findeSumButton.backgroundColor = .brown
+        nameLabel.layer.masksToBounds = true
+        nameLabel.layer.cornerRadius = 10
+        resultLabel.layer.masksToBounds = true
+        resultLabel.layer.cornerRadius = 10
+        buttonStackLabel.layer.masksToBounds = true
+        buttonStackLabel.layer.cornerRadius = 10
     }
 
 
